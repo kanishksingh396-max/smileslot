f516d136fa72c0faa2b128ae1c75d31fe7829837
Kanishk Singh <kanishksingh396@gmail.com>
2025-10-21 18:35:02 +0000

    Remove Patients button from the top of the home page

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -29,12 +29,6 @@ export function AppLayout({ children }: AppLayoutProps) {
                 <span className="text-xl font-semibold font-headline">SmileSlot</span>
             </Link>
           </div>
-          <Button asChild>
-            <Link href="/patients">
-              <Users className="mr-2 h-4 w-4" />
-              Patients
-            </Link>
-          </Button>
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6 lg:p-8">
           {children}
